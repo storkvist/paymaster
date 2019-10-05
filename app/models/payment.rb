394e9ba08aa0e1077494fd0cb6bb3a307572d9c5
Vitaly Shlyaga <vitaly@shlyaga.ru>
2019-10-05 23:07:57 +0300

    skip validation of empty payment fields

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,5 +2,5 @@ class Payment < ApplicationRecord
   validates_numericality_of %i[
     account_code bank_code counterparty_account_number counterparty_bank_bic counterparty_inn
     counterparty_kpp payment_number tax_info_kbk tax_info_okato tax_info_status
-  ]
+  ], allow_nil: true
 end
